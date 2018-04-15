ada72f7c9930390e2439bc72fe38e8d62e1a23fb
Fogel <fogel@email.com>
2018-04-14 21:23:40 -0400

    Lab 8 - added code for new message item; integrated with item 1

--- app/src/main/java/com/example/androidlabs/TestToolbarActivity.java
+++ app/src/main/java/com/example/androidlabs/TestToolbarActivity.java
@@ -8,14 +8,18 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class TestToolbarActivity extends AppCompatActivity {
 
     private static final String ACTIVITY_NAME = "TestToolbarActivity";
+
+    private String savedMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,7 @@ public class TestToolbarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_toolbar);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        savedMessage = "";
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +48,8 @@ public class TestToolbarActivity extends AppCompatActivity {
         switch (mi.getItemId()) {
             case R.id.action_one:
                 Log.d(ACTIVITY_NAME, "action_one");
-                Snackbar.make(findViewById(R.id.toolbar), "You clicked action one!", Snackbar.LENGTH_LONG)
+                String textToDisplay = (savedMessage == null || savedMessage.isEmpty())?"You clicked action one!":savedMessage;
+                Snackbar.make(findViewById(R.id.toolbar), textToDisplay, Snackbar.LENGTH_LONG)
                         .setAction("Action one", null).show();
                 break;
             case R.id.action_two:
@@ -70,6 +76,43 @@ public class TestToolbarActivity extends AppCompatActivity {
                 break;
             case R.id.action_three:
                 Log.d(ACTIVITY_NAME, "action_three");
+                AlertDialog.Builder builderThree = new AlertDialog.Builder(this);
+                // Get the layout inflater
+                LayoutInflater inflater = this.getLayoutInflater();
+
+                // This intermediate variable is necessary to be able to get R.id.newMessageEditText
+                // later. Otherwise, it is always null.
+                final View customDialogLayout = inflater.inflate(R.layout.dialog_three, null);
+
+                // Inflate and set the layout for the dialog
+                // Pass null as the parent view because its going in the dialog layout
+                builderThree.setView(customDialogLayout)
+                        // Add action buttons
+                        .setPositiveButton(R.string.newMessageText, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                EditText newMessageEditText = customDialogLayout.findViewById(R.id.newMessageEditText);
+
+                                if (newMessageEditText == null) {
+                                    Log.e(ACTIVITY_NAME, "newMessageEditText is null; WTF?");
+                                }
+                                else if (newMessageEditText.getText() == null) {
+                                    Log.e(ACTIVITY_NAME, "newMessageEditText.getText() is null; WTF?");
+                                }
+                                else {
+                                    savedMessage = newMessageEditText.getText().toString();
+                                    Log.i(ACTIVITY_NAME, "User clicked New Message. Saving: "+savedMessage);
+                                }
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.i(ACTIVITY_NAME, "User cancelled");
+                                // User cancelled the dialog
+                            }
+                        });
+                AlertDialog dialogThree = builderThree.create();
+                dialogThree.show();
                 break;
             case R.id.action_about:
 
