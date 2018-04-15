6a9fcefa74fb49fec6b65771049241a0b14606b4
Fogel <fogel@email.com>
2018-04-14 20:28:26 -0400

    Lab 8 - WIP; added code for first and second toolbar items

--- app/src/main/java/com/example/androidlabs/TestToolbarActivity.java
+++ app/src/main/java/com/example/androidlabs/TestToolbarActivity.java
@@ -1,8 +1,10 @@
 package com.example.androidlabs;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -41,12 +43,33 @@ public class TestToolbarActivity extends AppCompatActivity {
         switch (mi.getItemId()) {
             case R.id.action_one:
                 Log.d(ACTIVITY_NAME, "action_one");
+                Snackbar.make(findViewById(R.id.toolbar), "You clicked action one!", Snackbar.LENGTH_LONG)
+                        .setAction("Action one", null).show();
                 break;
             case R.id.action_two:
-                Log.d(ACTIVITY_NAME, "action_one");
+                Log.d(ACTIVITY_NAME, "action_two");
+                // Use the Builder class for convenient dialog construction
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.dialog_fire_missiles)
+                        .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.i(ACTIVITY_NAME, "User fired missiles! What a jerk!");
+                                // FIRE ZE MISSILES!
+                                finish();
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.i(ACTIVITY_NAME, "User cancelled");
+                                // User cancelled the dialog
+                            }
+                        });
+                // Create the AlertDialog object and return it
+                AlertDialog missilesDialog = builder.create();
+                missilesDialog.show();
                 break;
             case R.id.action_three:
-                Log.d(ACTIVITY_NAME, "action_one");
+                Log.d(ACTIVITY_NAME, "action_three");
                 break;
             case R.id.action_about:
 
